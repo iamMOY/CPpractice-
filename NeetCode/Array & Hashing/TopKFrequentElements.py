82573fa9c45d19aee72fa04ff90a4dcfb2fc7f74
Aditya Singh <62707925+pupscub@users.noreply.github.com>
2024-03-15 02:11:51 -0400

    Optimal Solution Included

--- NeetCode/Array & Hashing/TopKFrequentElements.py
+++ NeetCode/Array & Hashing/TopKFrequentElements.py
@@ -8,7 +8,26 @@ class Solution:
         sorted_frequency = sorted(frequency_count.items(), key = lambda x: x[1], reverse= True)
         return [x[0] for x in sorted_frequency[:k]]
     
+    def OptimalSolution(self,nums: list[int], k: int) -> list[int]:
+        count = {}
+        frequency = [[] for i in range(len(nums)+1)]
+
+        for num in nums:
+            count[num] = count.get(num,0)+1
+        
+        for num, freq in count.items():
+            frequency[freq].append(num)
+        res = []
+        for i in range(len(frequency)-1, 0,-1):
+            for num in frequency[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+    
 
 if __name__ == '__main__':
     solution = Solution()
-    ans = solution.TopKFrequent(nums = [1,1,1,3,3,3,3,4,4,4,4,4,4,4,4],k=2)
+    ans = solution.TopKFrequent(nums = [1,2,3,3,3,4,4,4,5,5,5],k=2)
+    ans2= solution.OptimalSolution(nums = [1,2,3,3,3,4,4,4,5,5,5],k=2)
+    print(ans)
+    print(ans2)
