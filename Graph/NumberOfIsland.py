ce6a50285f5012949f3a5b128642f5c36232203e
Aditya Singh <beast@Adityas-MacBook-Pro.local>
2024-03-31 21:10:52 -0400

    Included DFS

--- Graph/NumberOfIsland.py
+++ Graph/NumberOfIsland.py
@@ -5,7 +5,28 @@ class Solution:
         rows, columns = len(grid), len(grid[0])
         island = 0
         visited = set()
-        
+
+        def dfs(r,c):
+            q = collections.deque()
+            q.append((r,c))
+            visited.add((r,c))
+            while q:
+                # instead of popleft we just pop (stack)
+                row , col = q.pop()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                
+                for dr, dc in directions:
+                    r = row + dr
+                    c = col + dc
+                    if(
+                        r in range(rows) and 
+                        c in range(columns) and 
+                        grid[r][c] == '1' and 
+                        (r,c) not in visited
+                    ):
+                        visited.add((r,c))
+                        q.append((r,c))
+
         def bfs(r,c):
             q = collections.deque()
             q.append((r,c))
@@ -31,6 +52,8 @@ class Solution:
             for c in range(columns):
                 if grid[r][c]=='1' and (r,c) not in visited:
                     bfs(r,c)
+                    # uncomment to use dfs
+                    # dfs(r,c)
                     island +=1
         
         return island
