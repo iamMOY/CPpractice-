5ac5aee3d1a58c2e0c1a1b34a1e3b615fa96ede9
Aditya Singh <beast@Adityas-MacBook-Pro.local>
2024-04-07 18:04:17 -0400

    Added another approach to Rotting oranges

--- Graph/RottingOranges.py
+++ Graph/RottingOranges.py
@@ -40,9 +40,49 @@ class Solution:
 
         return max(max(row) for row in rotten_times) if fresh_count == 0 else -1
     
+# # approach 2: No need to maintain any grid for rotten_time 
+    
+#     def orangesRotting2(self, grid: list[list[int]]) -> int:
+#         time = 0
+#         rows, columns = len(grid), len(grid[0])
+#         q = collections.deque()
+#         fresh = 0
+
+#         for r in range(rows):
+#             for c in range(columns):
+#                 if grid[r][c] == 2:
+#                     q.append((r,c))
+#                 elif grid[r][c]==1:
+#                     fresh+=1
+        
+#         if fresh == 0:
+#             return 0
+        
+#         directions = [[1,0],[-1,0],[0,1],[0,-1]]
+#         while q and fresh>0:
+#             for i in range(len(q)):
+#                 r,c = q.popleft()
+#                 for dr, dc in directions:
+#                     r_new = r + dr
+#                     c_new = c+ dc
+#                     if(
+#                         r_new < 0 or r_new == rows or
+#                         c_new <0 or c_new == columns or
+#                         grid[r_new][c_new] != 1
+#                     ):
+#                         continue
+#                     grid[r_new][c_new] = 2
+#                     q.append((r_new,c_new))
+#                     fresh-=1
+
+#             time+=1
+        
+#         return time if fresh == 0 else -1
 
 if __name__ == "__main__":
     solution = Solution()
-    grid = [[2,1,1],[1,1,0],[0,1,1]]
+    # grid = [[2,1,1],[1,1,0],[0,1,1]]
+    grid = 
+    grid = 
     minutes = solution.orangesRotting(grid)
     print(minutes)
